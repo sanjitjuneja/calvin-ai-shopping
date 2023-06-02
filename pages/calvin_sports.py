03946624653fc7170c3c6bef1e25c17305289edd
Sanjit Juneja <sanjit.j55@gmail.com>
2023-06-01 19:02:44 -0500

    More work

--- pages/calvin_sports.py
+++ pages/calvin_sports.py
@@ -1,4 +1,5 @@
 # IMPORTS
+import time
 import streamlit as st
 from streamlit_chat import message
 from langchain.chains import ConversationChain
@@ -6,7 +7,11 @@ from langchain.chains.conversation.memory import ConversationEntityMemory
 from langchain.chains.conversation.prompt import \
     ENTITY_MEMORY_CONVERSATION_TEMPLATE
 from langchain.chat_models import ChatOpenAI
-
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
 
 # PAGE CONFIGURATION
 st.set_page_config(page_title="Calvin: Sports AI Bot 🏀", layout="centered")
@@ -14,6 +19,7 @@ st.set_page_config(page_title="Calvin: Sports AI Bot 🏀", layout="centered")
 # APP LAYOUT CONFIGURATION
 st.image("assets/calvin.png", width=150)
 st.title("Calvin: Sports AI Bot 🏀")
+status_bar = st.text(" ")
 st.markdown(
     """ 
         > :black[**Hey There, I'm Calvin 👋**]
@@ -21,6 +27,7 @@ st.markdown(
         > :black[*🧠 The cool thing about me is that I can remember things from all our conversations really well!*]
         """
 )
+st.text("---")
 
 # INITIALIZE SESSION STATES
 if "generated" not in st.session_state:
@@ -31,27 +38,6 @@ if "input" not in st.session_state:
     st.session_state["input"] = ""
 if "stored_session" not in st.session_state:
     st.session_state["stored_session"] = []
-
-
-
-
-
-# GET USER INPUT
-def get_text():
-    """
-    Get the user input text.
-
-    Returns:
-        (str): The text entered by the user
-    """
-    input_text = st.text_input(
-        "You: ",
-        st.session_state["input"],
-        key="input",
-        placeholder="Ask me anything ...",
-        label_visibility="hidden",
-    )
-    return input_text
 
 
 # STARTS A NEW CHAT
@@ -70,11 +56,13 @@ def new_chat():
     st.session_state.entity_memory.entity_store = {}
     st.session_state.entity_memory.buffer.clear()
 
+def clear_history():
+    del st.session_state.stored_session
 
 
 
 # SIDEBAR: MODEL SETTINGS
-with st.sidebar.expander(" 🧠 Model Settings ", expanded=False):
+with st.sidebar.expander(" 🛠️ Model Settings ", expanded=False):
     # OpenAI API KEY
     API_O = st.text_input(
         ":blue[[OpenAI API KEY](https://openai.com/blog/openai-api)]",
@@ -93,90 +81,99 @@ with st.sidebar.expander(" 🧠 Model Settings ", expanded=False):
     )
     # MODEL TEMPERATURE
     TEMP = st.slider(
-        "Randomness", min_value=0.0, max_value=1.0, step=0.01, format='%f'
+        "Randomness", min_value=0.0, max_value=1.0, step=0.01, format='%f, default=0.5'
     )
     
-# SIDEBAR: APP SETTINGS
-with st.sidebar.expander(" 🛠️ App Settings ", expanded=False):
-    # PREVIEW MEMORY STORE
-    if st.checkbox("Preview memory store"):
-        st.write(st.session_state.entity_memory.store)
-    # PREVIEW MEMORY BUFFER
-    if st.checkbox("Preview memory buffer"):
-        st.write(st.session_state.entity_memory.buffer)
+# # SIDEBAR: APP SETTINGS
+# with st.sidebar.expander(" 🛠️ App Settings ", expanded=False):
+#     # PREVIEW MEMORY STORE
+#     if st.checkbox("Preview memory store"):
+#         st.write(st.session_state.entity_memory.store)
+#     # PREVIEW MEMORY BUFFER
+#     if st.checkbox("Preview memory buffer"):
+#         st.write(st.session_state.entity_memory.buffer)
 
-# SIDEBAR: NEW CHAT BUTTON
+# SIDEBAR: NEW CHAT & CLEAR HISTORY BUTTON
 st.sidebar.button("New Chat", on_click=new_chat, type="primary")
+if st.session_state.stored_session:
+    st.sidebar.button("Clear History", on_click=clear_history, type="secondary")
 
-
-
-
-
-# Session state storage would be ideal
-if API_O:
-    # Create an OpenAI instance
-    llm = ChatOpenAI(temperature=TEMP, openai_api_key=API_O, model_name=MODEL, verbose=False)
-
-    # Create a ConversationEntityMemory object if not already created
-    if "entity_memory" not in st.session_state:
-        st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=5)
-
-    # Create the ConversationChain object with the specified configuration
-    Conversation = ConversationChain(
-        llm=llm,
-        prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-        memory=st.session_state.entity_memory,
-    )
-else:
-    st.warning(
-        "API KEY REQUIRED: Please open 'Model Settings' on the right sidebar and enter your OpenAI API KEY."
-    )
-
-
-
-# APP LOGISTICS
-user_input = None
-
-
-# GET USER INPUT FROM TEXT_INPUT
-if API_O:
-	user_input = get_text()
-
-# Generate the output using the ConversationChain object and the user input, and add the input/output to the session
-if user_input:
-    output = Conversation.run(input=user_input)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-
-
-
-
-
-
-
-# Allow to download as well
-download_str = []
-# Display the conversation history using an expander, and allow the user to download it
-with st.expander("Conversation", expanded=True):
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        st.info(st.session_state["past"][i], icon="👨")
-        st.success(st.session_state["generated"][i], icon="🤖")
-        download_str.append(st.session_state["past"][i])
-        download_str.append(st.session_state["generated"][i])
-
-    # Can throw error - requires fix
-    download_str = "\n".join(download_str)
-    if download_str:
-        st.download_button("Download", download_str)
-
-# Display stored conversation sessions in the sidebar
+# SIDEBAR: DISPLAY STORED SESSIONS
 if st.session_state.stored_session:
     for i, sublist in enumerate(st.session_state.stored_session):
         with st.sidebar.expander(label=f"Conversation {i+1}:"):
             st.write(sublist)
 
-# Allow the user to clear all stored conversation sessions
-if st.session_state.stored_session:
-    if st.sidebar.checkbox("Clear-all"):
-        del st.session_state.stored_session
+
+
+
+
+def get_text():
+    """
+    Gets text input from user.
+    """
+    # Get text input
+    user_input = st.text_input(
+        "You: ",
+        st.session_state["input"],
+        key="input",
+        placeholder="Type Here...",
+        label_visibility="hidden",
+    )
+    # Return input
+    return user_input
+
+def generate_response(user_input):
+    """
+    Generates response from user input.
+    """
+    # Get model
+    model = ChatOpenAI(
+        openai_api_key=API_O,
+        model_name=MODEL,
+        temperature=TEMP,
+        max_tokens=150,
+        stop=["\n", "User:", "Bot:"],
+    )
+    if "entity_memory" not in st.session_state:
+        st.session_state.entity_memory = ConversationEntityMemory(llm=model, k=5)
+    # MESSAGES
+    messages = [
+        SystemMessage(content="You are Calvin, a sports AI Assistant who helps answer any all questions related to sports."),
+        HumanMessage(content=user_input),
+    ]
+    # Send Response
+    response = model(messages)
+    # Return response
+    return response.content
+
+
+if not API_O:
+    st.warning("Please enter your OpenAI API key in the sidebar.")
+    user_input = None
+
+
+
+user_input = get_text()
+
+if user_input:
+    for i in range(15):
+        status_bar.progress(i, text="Sending...")
+        time.sleep(0.05)
+    st.text(" ")
+    # Get response
+    response = generate_response(user_input)
+    # Update status bar
+    for i in range(15, 100):
+        status_bar.progress(i, text="Generating...")
+        time.sleep(0.02)
+    status_bar.progress(100)
+    # Save response
+    st.session_state["generated"].append(response)
+    st.session_state["past"].append(user_input)
+    user_input = None
+
+if st.session_state["generated"]:
+    for i in range(len(st.session_state["generated"])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["past"][i], is_user=True, key=str(i)+"_user")
