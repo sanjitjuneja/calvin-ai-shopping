35aad97f5e9f336322d8e116b7e901f9dfe7de73
Sanjit Juneja <sanjit.j55@gmail.com>
2023-06-03 20:23:42 -0500

    Updates for db and auth

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import random
 import time
 from typing import Optional
+import sqlite3
 import streamlit as st
 from streamlit_chat import message
 import streamlit_authenticator as stauth
@@ -22,6 +23,12 @@ from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
 from langchain import OpenAI, SerpAPIWrapper, LLMChain
 from langchain.experimental import BabyAGI
 
+import yaml
+from yaml.loader import SafeLoader
+
+os.environ["OPENAI_API_KEY"] = "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"
+os.environ["SERPAPI_API_KEY"] = "fc35ee3159ee64b6f23fa05b2083b87e6a6ccd9178f961b2e4196ce5f7b510ae"
+
 
 # SETUP: PAGE CONFIGURATION
 st.set_page_config(page_title="Calvin: AI Shopper", page_icon="assets/calvin.png", layout="centered", initial_sidebar_state="auto")
@@ -36,9 +43,6 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 # AUTHENTICATION SETUP
-import yaml
-from yaml.loader import SafeLoader
-
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
@@ -49,24 +53,37 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
     config['preauthorized']
 )
+
 name, authentication_status, username = authenticator.login('Login', 'sidebar')
 
 
-# SETUP: INITIALIZE SESSION STATES
-if "calvin_generated" not in st.session_state:
-    st.session_state["calvin_generated"] = []
-if "calvin_past" not in st.session_state:
-    st.session_state["calvin_past"] = []
-if "calvin_input" not in st.session_state:
-    st.session_state["calvin_input"] = ""
-if "calvin_stored_session" not in st.session_state:
-    st.session_state["calvin_stored_session"] = []
 
+# DATABASE SETUP
+conn = sqlite3.connect('data.db', check_same_thread=False)
+c = conn.cursor()
+c.execute("""CREATE TABLE IF NOT EXISTS responses(username, response)""")
+c.execute("""CREATE TABLE IF NOT EXISTS prompts(username, prompt)""")
+c.execute("""CREATE TABLE IF NOT EXISTS stored_sessions(username, sessions)""")
+conn.commit()
+
+
+
+# SETUP: INITIALIZE SESSION STATES
+if authentication_status:
+    if "responses" not in st.session_state:
+        st.session_state["responses"] = c.execute("""SELECT response FROM responses WHERE username = ?""", (username,)).fetchall()
+    if "prompts" not in st.session_state:
+        st.session_state["prompts"] = c.execute("""SELECT prompt FROM prompts WHERE username = ?""", (username,)).fetchall()
+    if "stored_sessions" not in st.session_state:
+        st.session_state["stored_sessions"] = c.execute("""SELECT sessions FROM stored_sessions WHERE username = ?""", (username,)).fetchall()
+    if "user_input" not in st.session_state:
+        st.session_state["user_input"] = ""
+    conn.close()
 
 # SETUP: APP LAYOUT CONFIGURATION
 st.image("assets/calvin.png", width=150)
 st.title("Calvin: Your AI Shopper 🛍️")
-if len(st.session_state["calvin_generated"]) > 0:
+if authentication_status and len(st.session_state["responses"]) > 0:
     status_bar = st.progress(100)
 else:
     status_bar = st.text(" ")
@@ -80,55 +97,54 @@ st.markdown(
 st.text("---")
 
 
-
-# FUNCTION: GET TEXT INPUT
-def calvin_get_text():
-    """
-    Gets text input from user.
-    """
-    # Get text input
-    user_input = st.text_input(
-        "Ask Calvin Anything:",
-        value=st.session_state["calvin_input"],
-        key="input",
-        placeholder="Type Here...",
-        label_visibility="hidden",
-    )
-    # Return input
-    return user_input
-
-
 # FUNCTION: START A NEW CHAT
 def new_chat():
     """
     Clears session state and starts a new chat.
     """
-    if st.session_state["calvin_generated"]:
+    if st.session_state["responses"]:
         save = []
-        for i in range(len(st.session_state["calvin_generated"]) - 1, -1, -1):
-            save.append("User:" + st.session_state["calvin_past"][i])
-            save.append("Bot:" + st.session_state["calvin_generated"][i])
-        st.session_state["calvin_stored_session"].append(save)
-        st.session_state["calvin_generated"] = []
-        st.session_state["calvin_past"] = []
-        st.session_state["calvin_input"] = ""
+        for i in range(len(st.session_state["responses"]) - 1, -1, -1):
+            save.append("User:" + st.session_state["prompts"][i])
+            save.append("Bot:" + st.session_state["responses"][i])
+        st.session_state["stored_sessions"].append(save)
+        conn = sqlite3.connect('data.db', check_same_thread=False)
+        c = conn.cursor()
+        c.execute("""INSERT INTO stored_sessions VALUES (?, ?)""", (username, str(save)))
+        c.execute("""DELETE FROM responses WHERE username = ?""", (username,))
+        c.execute("""DELETE FROM prompts WHERE username = ?""", (username,))
+        conn.commit()
+        conn.close()
+        st.session_state["responses"] = []
+        st.session_state["prompts"] = []
+        st.session_state["user_input"] = ""
 
 
 # FUNCTION: CLEAR CHAT HISTORY
 def clear_history():
-    del st.session_state.calvin_stored_session
+    conn = sqlite3.connect('data.db', check_same_thread=False)
+    c = conn.cursor()
+    c.execute("""DELETE FROM stored_sessions WHERE username = ?""", (username,))
+    c.execute("""DELETE FROM responses WHERE username = ?""", (username,))
+    c.execute("""DELETE FROM prompts WHERE username = ?""", (username,))
+    conn.commit()
+    conn.close()
+    del st.session_state.stored_sessions
+    del st.session_state.responses
+    del st.session_state.prompts
+
 
 # FUNCTION: TRY EXAMPLE
 def try_example():
     rand = random.randint(0, 2)
-    exampleStr = ""
+    example_prompt = ""
     if rand == 0:
-        exampleStr = "Search for the iPhone 14 Pro case with the most value."
+        example_prompt = "Search for the iPhone 14 Pro case with the most value."
     elif rand == 1:
-        exampleStr = "Formulate a shopping cart with all designer items with a total less than $1000"
+        example_prompt = "Formulate a shopping cart with all designer items with a total less than $1000"
     else:
-        exampleStr = "Find me a premium webcam good for video calls."
-    st.session_state["calvin_input"] = exampleStr
+        example_prompt = "Find me a premium webcam good for video calls."
+    st.session_state["user_input"] = example_prompt
 
 
 
@@ -199,16 +215,15 @@ if authentication_status:
 # SIDEBAR: CHAT HISTORY
 if authentication_status:
     st.sidebar.header("Chat History")
-    # newChatBut, clearHistoryBut = st.sidebar.columns([0.7, 0.7])
     st.sidebar.button("New Chat", on_click=new_chat, type="primary")
-    if st.session_state.calvin_stored_session:
+    if st.session_state.stored_sessions:
         st.sidebar.button("Clear History", on_click=clear_history, type="secondary")
     st.sidebar.text(" ")
 
     # SIDEBAR: DISPLAY STORED SESSIONS
-    if st.session_state.calvin_stored_session:
-        for i, sublist in enumerate(st.session_state.calvin_stored_session):
-            with st.sidebar.expander(label=sublist[0][5:]):
+    if st.session_state.stored_sessions:
+        for i, sublist in enumerate(st.session_state.stored_sessions):  
+            with st.sidebar.expander(label=f"Conversation {i+1}", expanded=False):
                 st.write(sublist)
     st.sidebar.text(" ")
     st.sidebar.progress(0)
@@ -318,13 +333,13 @@ calvin = BabyAGI.from_llm(
 )
 
 def small():
-    st.session_state["calvin_input"] = calvin_user_input
+    st.session_state["user_input"] = text_input
 
 # MAIN: USER INPUT
 if authentication_status:
-    calvin_user_input = st.text_input(
+    text_input = st.text_input(
         "Ask Calvin Anything:",
-        st.session_state["calvin_input"],
+        st.session_state["user_input"],
         key="input",
         placeholder="Type Here...",
         label_visibility="hidden",
@@ -333,16 +348,16 @@ if authentication_status:
 
 
 # MAIN: PROCESS USER INPUT
-if st.session_state["calvin_input"] != "":
+if authentication_status and st.session_state["user_input"] != "":
     # Update status bar
-    for i in range(15):
+    for i in range(25):
         status_bar.progress(i, text="Sending...")
         time.sleep(0.05)
     st.text(" ")
 
     # Get response
     response = "This would be Calvin's response."
-    # response = calvin({"objective": calvin_user_input})
+    # response = calvin({"objective": st.session_state["user_input"]]})
 
     # Update status bar
     for i in range(15, 100):
@@ -351,17 +366,23 @@ if st.session_state["calvin_input"] != "":
     status_bar.progress(100)
 
     # Save response
-    st.session_state["calvin_generated"].append(response)
-    st.session_state["calvin_past"].append(calvin_user_input)
+    st.session_state["responses"].append(response)
+    st.session_state["prompts"].append(st.session_state["user_input"])
+    conn = sqlite3.connect('data.db', check_same_thread=False)
+    c = conn.cursor()
+    c.execute("""INSERT INTO responses VALUES (?, ?)""", (username, response))
+    c.execute("""INSERT INTO prompts VALUES (?, ?)""", (username, st.session_state["user_input"]))
+    conn.commit()
+    conn.close()
 
     # Reset user input
-    st.session_state["calvin_input"] = ""
+    st.session_state["user_input"] = ""
 
 
 
 
 # MAIN: DISPLAY CHAT HISTORY
-if authentication_status and st.session_state["calvin_generated"]:
-    for i in range(len(st.session_state["calvin_generated"])-1, -1, -1):
-        message(st.session_state["calvin_generated"][i], key=str(i))
-        message(st.session_state["calvin_past"][i], is_user=True, key=str(i)+"_user")
+if authentication_status and st.session_state["responses"]:
+    for i in range(len(st.session_state["responses"])-1, -1, -1):
+        message(st.session_state["responses"][i], key=str(i))
+        message(st.session_state["prompts"][i], is_user=True, key=str(i)+"_user")
